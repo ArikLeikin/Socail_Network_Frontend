bd28ca5b499ad824bd5d56a522dd3960cbe4810d
Arik <arik.l@hiredscore.com>
2024-03-10 16:37:02 +0200

    post list editing added to show by last first

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,17 +10,54 @@ import "react-toastify/dist/ReactToastify.css";
 interface PostData {
   id: number;
   title: string;
+  user: string;
   body: string;
+  picture: string;
+  createdAt: Date;
 }
+
+interface UserData {
+  email: string;
+}
+
+const getFormattedDateTime = (dateString: string) => {
+  const date = new Date(dateString);
+  const options = { hour: "numeric", minute: "numeric", hour12: false };
+  const formattedDate = date.toLocaleDateString(undefined, options);
+  return formattedDate;
+};
 
 const Home: React.FC = () => {
   const [posts, setPosts] = useState<PostData[]>([]);
   const [showError, setShowError] = useState(false);
-
+  const [users, setUsers] = useState<{ [key: string]: UserData }>({});
+  const user = JSON.parse(localStorage.getItem("user"));
   const handleAddPost = async (newPost: PostData) => {
-    console.log("Click Post");
+    try {
+      await fetchUserForPost(newPost.id, newPost.user);
+      setPosts([...posts, newPost]);
+    } catch (error) {
+      console.error("Error adding post:", error);
+    }
+  };
+  const fetchUserForPost = async (postId: number, userId: string) => {
+    try {
+      const response = await fetch(`http://localhost:3000/user/${userId}`, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${user.accessToken}`,
+        },
+      });
 
-    setPosts([...posts, newPost]);
+      if (response.ok) {
+        const userData: UserData = await response.json();
+        setUsers((prevUsers) => ({ ...prevUsers, [userId]: userData }));
+      } else {
+        toast.error("Error fetching user data!");
+      }
+    } catch (error) {
+      console.error("Error fetching user data:", error);
+    }
   };
 
   useEffect(() => {
@@ -31,35 +68,54 @@ const Home: React.FC = () => {
         });
 
         if (response.ok) {
-          console.log(response.body);
+          console.log("Successful");
+          const data: PostData[] = await response.json();
+          setPosts(data);
 
-          console.log("Succesful");
+          data.forEach((post) => {
+            fetchUserForPost(post.id, post.user);
+          });
         } else {
           toast.error("Error fetching posts!");
         }
-
-        const data: PostData[] = await response.json();
-        setPosts(data);
       } catch (error) {
         setShowError(true);
       }
     };
 
     fetchPosts();
-  }, []); // Empty dependency array ensures the effect runs only once on mount
+  }, []);
 
   return (
     <div className="d-flex flex-column align-items-center">
       <AddPost onAddPost={handleAddPost} />
-      {posts.map((post) => (
-        <div className="myWrapper mt-5" key={post.id}>
-          <div className="center"></div>
-          <div className="post-wrapper">
-            <h3>{post.title}</h3>
-            <p>{post.body}</p>
+      {posts
+        .slice()
+        .reverse()
+        .map((post) => (
+          <div
+            className="myWrapper mt-5"
+            key={post.id}
+            style={{ overflowWrap: "break-word" }}
+          >
+            <div className="center"></div>
+            <div className="post-wrapper d-flex flex-column align-items-center">
+              <div className="d-flex flex-column align-items-center mt-2">
+                <h6>{users[post.user]?.email}</h6>
+                <h6>{getFormattedDateTime(post.createdAt)}</h6>
+              </div>
+              <hr />
+              <p className="mt-2">{post.body}</p>
+              {post.picture && (
+                <img
+                  src={"http://localhost:3000/public/" + post.picture}
+                  alt=""
+                  style={{ maxWidth: "100%", height: "600px" }}
+                />
+              )}
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
     </div>
   );
 };
