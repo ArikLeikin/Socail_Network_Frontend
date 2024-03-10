923c079b4e913af9a7edf4b3e9687d692442a5e2
Arik <arik.l@hiredscore.com>
2024-03-11 00:24:49 +0200

    fixed anchor tag

--- src/pages/News.tsx
+++ src/pages/News.tsx
@@ -17,7 +17,8 @@ function News() {
         const resp = await axios.get(
           "https://newsapi.org/v2/everything?q=technology&apiKey=b738ed2669c54125aae96fba7c1107d5&pageSize=50"
         );
-        setNewsData(resp.data.articles);
+        const shuffledArticles = await shuffleArray(resp.data.articles);
+        setNewsData(shuffledArticles);
       } catch (error) {
         console.error("Error fetching news data:", error);
       } finally {
@@ -26,6 +27,14 @@ function News() {
     }
     getNewsData();
   }, []);
+
+  async function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
 
   return (
     <div className="App">
@@ -41,21 +50,24 @@ function News() {
           <Container>
             {newsData.map((newsItem, index) => (
               <Row className="d-flex justify-content-center" key={index}>
-                <Col xs={12} className="mt-5 w-700">
-                  <a
-                    className="d-flex justify-content-center"
-                    target="_blank"
-                    rel="noreferrer"
-                    href={newsItem.url}
-                  >
-                    <Card>
+                <Col
+                  xs={12}
+                  className="d-flex justify-content-center mt-5 w-700"
+                >
+                  <Card>
+                    <a
+                      className="d-flex flex-column justify-content-center"
+                      target="_blank"
+                      rel="noreferrer"
+                      href={newsItem.url}
+                    >
                       <Card.Title className="my-3">{newsItem.title}</Card.Title>
                       <Card.Img src={newsItem.urlToImage} />
                       <Card.Body>
                         <Card.Text>{newsItem.description}</Card.Text>
                       </Card.Body>
-                    </Card>
-                  </a>
+                    </a>
+                  </Card>
                 </Col>
               </Row>
             ))}
