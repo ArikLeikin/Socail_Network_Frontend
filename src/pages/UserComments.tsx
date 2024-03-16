cafb098b23b49c64bf53d4b660fd4fc0f53d552a
Dolev <dolevs1890@gmail.com>
2024-03-16 21:51:34 +0200

    adding click event for user comments

--- src/pages/UserComments.tsx
+++ src/pages/UserComments.tsx
@@ -2,9 +2,13 @@ import React, { useState, useEffect } from "react";
 import { Card, ListGroup } from "react-bootstrap";
 
 import moment from "moment";
+import { PostData } from "../components/post/Post";
+import { useNavigate } from "react-router-dom";
 const UserComments = () => {
   const id = JSON.parse(localStorage.getItem("user"))._id;
   const [comments, setComments] = useState([]);
+  const [selectedPost, setSelectedPost] = useState<PostData>();
+  const navigate = useNavigate();
   console.log("id", id);
 
   useEffect(() => {
@@ -18,45 +22,60 @@ const UserComments = () => {
           },
         }
       );
-      console.log("response", response);
+      // console.log("response", response);
 
       if (response.ok) {
         const commentsData = await response.json();
-        console.log("commentsData", commentsData);
         setComments(commentsData);
       }
     };
 
     fetchComments();
-  }, [id]);
+  }, [id, selectedPost]);
 
   if (!comments) {
     return <div>Loading...</div>;
   }
 
+  async function  handleCommentClick(postId: PostData): Promise<void> {
+    const response = await fetch(`http://localhost:3000/posts/${postId}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    
+    if (response.ok) {
+      console.log("response", response);
+      const postData = await response.json();
+      console.log("postData", postData);
+      setSelectedPost(postData);
+      navigate(`/comments/${postId}`);
+    }
+  }
+
   return (
     <>
-    <div className="card-highlight">
-      <h1 id="user-comment-highlight">User Comments</h1>
+      <div className="card-highlight">
+        <h1 id="user-comment-highlight">User Comments</h1>
       </div>
-    <div className="comment-card-container">
-      {comments.sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()).map((comment, index) => (
-        <Card key={index} style={{ width: "25rem", marginBottom: "1rem" }}>
-          <Card.Body>
-            <Card.Text>{comment.body}</Card.Text>
-          </Card.Body>
-          <ListGroup className="list-group-flush">
-            <ListGroup.Item>Posted on {moment(comment.createdAt).format('MMM Do YYYY, HH:mm:ss')}</ListGroup.Item>
-          </ListGroup>
-        </Card>   
-      ))}
+      <div className="comment-card-container">
+        {comments.sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()).map((comment, index) => (
+          <Card key={index} 
+          style={{ width: "25rem", marginBottom: "1rem" }} 
+          onClick={() => handleCommentClick(comment.post)}>
+            <div>
+         
+            </div>
+            <Card.Body>
+              <Card.Text>{comment.body}</Card.Text>
+            </Card.Body>
+            <ListGroup className="list-group-flush">
+              <ListGroup.Item>Posted on {moment(comment.createdAt).format('MMM Do YYYY, HH:mm:ss')}</ListGroup.Item>
+            </ListGroup>
+          </Card>   
+        ))}
       </div>
-    
-   
-  
-   
-
-
     </>
   );
 }
