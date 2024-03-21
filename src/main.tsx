7a8d681ee86688d0135dd4099ca5342734367c38
Dolev Shmueli <dolevs1890@gmail.com>
2024-03-21 17:48:44 +0200

    editing

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import "./index.css";
 import App from "./App";
 import { GoogleOAuthProvider } from '@react-oauth/google';
-
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <GoogleOAuthProvider
-    clientId="'145536041124-4orvmru8ark9cljg0lad4scb8tpsjlnm.apps.googleusercontent.com'">
+    clientId="145536041124-4orvmru8ark9cljg0lad4scb8tpsjlnm.apps.googleusercontent.com">
     <React.StrictMode>
       <App />
     </React.StrictMode>
